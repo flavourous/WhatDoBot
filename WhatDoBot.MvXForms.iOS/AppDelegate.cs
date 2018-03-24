e75eb46f84748cf3e81f27f70b02c5112fdc8f36
David Higgins <d_higgins@imap.cc>
2018-03-24 23:04:58 +0000

    added ios app

---
+++ WhatDoBot.MvXForms.iOS/AppDelegate.cs
@@ -0,0 +1,117 @@
+﻿using CoreGraphics;
+using Foundation;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter.Distribute;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Forms.iOS;
+using MvvmCross.Platform;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using UIKit;
+
+namespace WhatDoBot.MvXForms.iOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : MvxFormsApplicationDelegate
+    {
+        public override UIWindow Window { get; set; }
+
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            SemaphoreSlim s = new SemaphoreSlim(0, 1);
+            Crashes.FailedToSendErrorReport += (o, e) => s.Release();
+            Crashes.SentErrorReport += (o, e) => s.Release();
+            AppCenter.Start("AAPP", typeof(Analytics), typeof(Crashes), typeof(Distribute));
+            Distribute.DontCheckForUpdatesInDebug();
+            AppDomain.CurrentDomain.UnhandledException += (o, e) => Console.WriteLine("Exception Raised{0}----------------{0}{1}", Environment.NewLine, e.ExceptionObject);
+            Window = new UIWindow(UIScreen.MainScreen.Bounds);
+
+            Distribute.ReleaseAvailable = OnReleaseAvailable;
+
+            if (Crashes.HasCrashedInLastSessionAsync().Result)
+            {
+                var sc = new SplashController();
+                Window.RootViewController = sc;
+                Window.MakeKeyAndVisible();
+                Task.Run(async () =>
+                {
+                    await s.WaitAsync(10000);
+                    BeginInvokeOnMainThread(StartMvvMxForms);
+                });
+            }
+            else StartMvvMxForms();
+            return true;
+        }
+
+        bool OnReleaseAvailable(ReleaseDetails releaseDetails)
+        {
+            var title = "Version " + releaseDetails.ShortVersion + " available!";
+            var alert = UIAlertController.Create(title, releaseDetails.ReleaseNotes, UIAlertControllerStyle.Alert);
+            alert.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, x => Distribute.NotifyUpdateAction(UpdateAction.Update)));
+            if (!releaseDetails.MandatoryUpdate)
+                alert.AddAction(UIAlertAction.Create("Cancel", UIAlertActionStyle.Cancel, x => Distribute.NotifyUpdateAction(UpdateAction.Postpone)));
+            return true;
+        }
+
+
+        void StartMvvMxForms()
+        {
+            Window = new UIWindow(UIScreen.MainScreen.Bounds);
+
+            var setup = new Setup(this, Window);
+            setup.Initialize();
+
+            var startup = Mvx.Resolve<IMvxAppStart>();
+            startup.Start();
+
+            LoadApplication(setup.FormsApplication);
+
+            Window.MakeKeyAndVisible();
+        }
+    }
+    public class SplashController : UIViewController
+    {
+        UILabel load, sad;
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            Title = "Starting Up";
+            View.BackgroundColor = UIColor.Purple;
+
+            // keep the code the username UITextField
+            load = new UILabel
+            {
+                TextAlignment = UITextAlignment.Center,
+                TextColor = UIColor.White,
+                Text = "uploading crash data"
+            };
+            sad = new UILabel
+            {
+                TextAlignment = UITextAlignment.Center,
+                TextColor = UIColor.White,
+                Text = "(._.)"
+            };
+
+            load.Font = UIFont.BoldSystemFontOfSize(24f);
+            sad.Font = UIFont.SystemFontOfSize(24f);
+
+            View.AddSubview(load);
+            View.AddSubview(sad);
+        }
+        public override void ViewDidLayoutSubviews()
+        {
+            base.ViewDidLayoutSubviews();
+            nfloat h = 31.0f;
+            nfloat w = View.Bounds.Width;
+            nfloat t = View.Bounds.Height / 2;
+            load.Frame = new CGRect(10, t - h, w - 20, h);
+            sad.Frame = new CGRect(10, t, w - 20, h);
+        }
+    }
+}
+
+
---
+++ WhatDoBot.MvXForms.iOS/Main.cs
@@ -0,0 +1,82 @@
+﻿using MvvmCross.Core.ViewModels;
+using MvvmCross.Forms.Platform;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UIKit;
+
+namespace WhatDoBot.MvXForms.iOS
+{
+    public class Application
+    {
+        // This is the main entry point of the application.
+        static void Main(string[] args)
+        {
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+    }
+    public class LinkerPleaseInclude
+    {
+        public void Include(ConsoleColor color)
+        {
+            Console.Write("");
+            Console.WriteLine("");
+            color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+        }
+    }
+
+    public class Setup : MvxFormsIosSetup
+    {
+        public Setup(IMvxApplicationDelegate applicationDelegate, UIWindow window)
+            : base(applicationDelegate, window)
+        {
+        }
+
+        //protected override void PerformBootstrapActions()
+        //{
+        //    base.PerformBootstrapActions();
+
+        //    PluginLoader.Instance.EnsureLoaded();
+        //}
+
+
+        protected override MvxFormsApplication CreateFormsApplication()
+        {
+            return new XamarinForms.App();
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+            return new Core.App();
+        }
+
+        protected override IEnumerable<Assembly> GetViewModelAssemblies()
+        {
+            var b = base.GetViewModelAssemblies();
+            return b.Concat(new[] { typeof(Core.ViewModels.MainPageViewModel).Assembly });
+        }
+
+        protected override IEnumerable<Assembly> GetViewAssemblies()
+        {
+            var b = base.GetViewAssemblies();
+            return b.Concat(new[] { typeof(XForms.MainPage).Assembly });
+        }
+
+        protected override IMvxTrace CreateDebugTrace()
+        {
+            return base.CreateDebugTrace();
+        }
+        protected override IMvxLogProvider CreateLogProvider()
+        {
+            return base.CreateLogProvider();
+        }
+    }
+}
