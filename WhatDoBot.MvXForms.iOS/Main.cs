7d389dd21c06a1d78d9751e9233e6dc2042e6f53
David Higgins <d_higgins@imap.cc>
2018-04-05 08:52:48 +0100

    ok, lets add stuff to linkerpleaseinclude see if we can get it running

--- WhatDoBot.MvXForms.iOS/Main.cs
+++ WhatDoBot.MvXForms.iOS/Main.cs
@@ -22,21 +22,6 @@ namespace WhatDoBot.MvXForms.iOS
             UIApplication.Main(args, null, "AppDelegate");
         }
     }
-    public class LinkerPleaseInclude
-    {
-        public void Include(ConsoleColor color)
-        {
-            Console.Write("");
-            Console.WriteLine("");
-            color = Console.ForegroundColor;
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.ForegroundColor = ConsoleColor.Gray;
-            Console.ForegroundColor = ConsoleColor.DarkGray;
-        }
-    }
 
     public class Setup : MvxFormsIosSetup
     {
